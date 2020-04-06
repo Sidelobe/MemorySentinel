1a799a92ce6720e37c0c65cad4bb4efcde26d3fe
Lorenz Bucher <lorenz.bucher@gmail.com>
2020-04-06 16:11:52 +0200

    disable tests if GLIBC

--- source/MemorySentinel.cpp
+++ source/MemorySentinel.cpp
@@ -119,8 +119,7 @@ void operator delete[](void* ptr) noexcept
 }
 
 // TODO: This *should* work for GLIBC, however, symbols are unresolved in TravisCI environment - that's why we disable it for now
-#define DISABLE_IF_GLIBC 1
-#if (defined(__clang__) || defined(__GNUC__)) && !defined(DISABLE_IF_GLIBC)
+#if (defined(__clang__) || defined(__GNUC__)) && !defined(__GLIBC__)
 
 static void* (*builtinMalloc)(size_t) = nullptr;
 static void* (*builtinCalloc)(size_t, size_t) = nullptr;
